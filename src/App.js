28f097237adef398478645d315d797332f211280
Andrew Allen <asallen67@gmail.com>
2019-10-02 16:46:22 -0400

    sorting options form completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { key } from './api_key';
 import { proxy_url, SL_People_url } from './constants';
-import './stylesheets/App.css';
+import SalesLoft_banner from './images/SalesLoft_banner.png'
+import './stylesheets/App.scss';
 
 
 class App extends React.Component {
@@ -9,16 +10,18 @@ class App extends React.Component {
   state = {
     people: [],
     page: 1,
+    next_page: null,
     per_page: 50,
     sort_by: "created_at",
-    sort_direction: "ASC"
+    sort_direction: "ASC",
+    showCharStats: false
   }
 
   componentDidMount() {
-    this.fetchPeople()
+    // this.fetchPeople()
   }
 
-  fetchPeople = ()=> {
+  fetchPeople = () => {
     let full_url = [
       proxy_url,
       SL_People_url,
@@ -38,16 +41,56 @@ class App extends React.Component {
     })
     .then(res => res.json())
     .then(res => {
-      console.log(res)
-      this.setState({ people: res.data }) 
+      // console.log(res)
+      if (res.data) {
+        this.setState({ 
+          people: res.data, 
+          next_page: res.metadata.paging.next_page
+        }) 
+      }
     })
+  }
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value })
+  handleSubmit = e => {
+    e.preventDefault()
+    this.setState({ showCharStats: false })
+    this.fetchPeople()
   }
 
 
   render() {
     return (
       <div className="App">
-        Hello, world.
+        <img className="banner" src={SalesLoft_banner} alt="SalesLoft-Banner" draggable="false" />
+        <br/>
+        <form className="sort-form" onSubmit={this.handleSubmit}>
+
+          <label>Sort By</label>
+          <select name="sort_by" className="select" value={this.state.sort_by} onChange={this.handleChange}>
+            <option value="created_at">Date of Entry (ID)</option>
+            <option value="updated_at">Date of last Update</option>
+            <option value="last_contacted_at">Date of Last Contact</option>
+          </select>
+          <br/>
+          <label>Order</label>
+          <select name="sort_direction" className="select" value={this.state.sort_direction} onChange={this.handleChange}>
+            <option value="ASC">Ascending (small to large)</option>
+            <option value="DESC">Descending (large to small)</option>
+          </select>
+          <br/>
+          
+          <div className="center-contents">
+            <label>{ this.state.per_page } items per page</label>
+            <br/>
+            <input type="range" name="per_page" min="1" max="100" value={this.state.per_page} onChange={this.handleChange} />
+          </div>
+
+          <div className="center-contents">
+            <button className="blue-btn submit" type="submit">Apply Sorting Options</button>
+          </div>
+        </form>
+        <br/>
       </div>
     )
   }
